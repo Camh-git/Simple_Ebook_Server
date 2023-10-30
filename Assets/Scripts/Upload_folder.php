071f5013a5ec39f1b74caaaf1b28ff9ffa61bd4b
Camh-git <cameronhillier1@gmail.com>
2023-10-30 23:05:26 +0000

    Fix: fixed upload folder
    Modified the folder upload php script so that it actually creates folders and puts uploads in the correct location.
    Installed a python formatter, which had a go at formatting the API.
    Will investigate converting this and the single book upload to be part
    of the API.

--- Assets/Scripts/Upload_folder.php
+++ Assets/Scripts/Upload_folder.php
@@ -1,34 +1,33 @@
 <?php
-    //$files = array_filter($_FILES['inpFiles'],$_POST['UF_name']); 
-
     //Create a folder for the books, using the name the user provided
-    //TODO: get new folder name, set up try catch
     if(isset($_FILES['UF_inp_Files']))
     {
-      $folder = "../../Books/";
-      mkdir($folder);
+      try{
+        $folder = "../../Books/".$_POST["UF_name"]."/";
+        mkdir($folder);
 
-      //upload the files
-      $total = count($_FILES['UF_inp_Files']['name']);
-      for( $i=0 ; $i < $total ; $i++ ) {
+        //upload the files
+        $total = count($_FILES['UF_inp_Files']['name']);
+        for( $i=0 ; $i < $total ; $i++ ) {
 
-        //Get the temp file path
-        $tmpFilePath = $_FILES['UF_inp_Files']['tmp_name'][$i];
+          //Get the temp file path
+          $tmpFilePath = $_FILES['UF_inp_Files']['tmp_name'][$i];
       
-        //Make sure we have a file path
-        if ($tmpFilePath != ""){
-          //Setup our new file path
-          $newFilePath = $folder . $_FILES['UF_inp_Files']['name'][$i];
+          //Make sure we have a file path
+          if ($tmpFilePath != ""){
+            //Setup our new file path
+            $newFilePath = $folder . $_FILES['UF_inp_Files']['name'][$i];
       
-          //Upload the file into the temp dir
-          if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-            //Handle  here
-      
+            //Upload the file to it's dir
+            if(move_uploaded_file($tmpFilePath, $newFilePath)) {} 
           }
         }
       }
-    }
-    else{
-      echo("Input file not set, this is likely because the file is too large, the limit is ~30MB");
-    }
+      catch(Exception $e) {
+        echo $e->getMessage();
+      }
+  }
+  else{
+    echo("Input file not set, this is likely because the file is too large, the limit is ~30MB");
+  }
 ?>
