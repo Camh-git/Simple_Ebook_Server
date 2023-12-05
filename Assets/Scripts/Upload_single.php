f2747349678cbf7a4b0817cf7ae6194c77901b40
Camh-git <cameronhillier1@gmail.com>
2023-12-05 22:16:27 +0000

    Chore: added "please move" message to upload book
    Upload single's confirmation page now tells you to move the book out of the uploads folder into a permanent folder.
    Made upload single's "Book uploaded to : uploads/ x"  message a h3 and added some spacing for better readability.
    Updated todo

--- Assets/Scripts/Upload_single.php
+++ Assets/Scripts/Upload_single.php
@@ -8,7 +8,7 @@
     
       if(copy($file['tmp_name'],$targetPath))
       {
-        echo("Book uploaded to: Uploads/".basename($_FILES["US_file_inp"]["name"]));
+        echo("<br><br><h3>Book uploaded to: Uploads/".basename($_FILES["US_file_inp"]["name"])."</h3>");
       }
       //Send the user to the success screen
       readfile("../Confirmation_messages/Upload_book_success.html");
