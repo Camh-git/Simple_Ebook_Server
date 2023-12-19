6763db04351e6a24f7874e158b2ba0dfe574e3d5
Camh-git <cameronhillier1@gmail.com>
2023-12-19 23:01:11 +0000

    Refactor: managment select json
    Refactoring management selects to use the results of the API calls directly.
    Still WIP
    needs debugging (see todos) and testing to ensure all the API functions still work.

--- Assets/Scripts/Populate_management_selects.js
+++ Assets/Scripts/Populate_management_selects.js
@@ -1,89 +1,58 @@
+async function get_map(url) {
+  try {
+    const req = await fetch(url);
+    const data = await req.json();
+    return data;
+  } catch {
+    console.log(`Failed to call:${url}, will use placeholders if possible`);
+    return 500;
+  }
+}
 async function Pop_management_selects() {
-  //This script collects book info and creates an object from it.
-  const Book_folder = document.getElementById("Book_collection");
-  const lib_content = await fetch(`http://192.168.1.110:5000/list-books`);
-  Book_folder.innerHTML = await lib_content.json();
+  //Get the thumnail and book lists
+  const THUMB_MAP = await get_map(`http://192.168.1.110:5000/thumb-map`);
+  const BOOK_MAP = await get_map(`http://192.168.1.110:5000/json-list-books`);
+  console.log(BOOK_MAP);
 
-  //Collect the thumbnail info in a similar manner
-  const Thumb_collection = document.getElementById("Thumb_collection");
-  const Thumb_content = await fetch(`http://192.168.1.110:5000/list-thumbs`);
-  Thumb_collection.innerHTML += await Thumb_content.json();
+  //Reset the selects and handle the folder selects
+  const SELECT_LIST = document.querySelectorAll("select"); //TODO: check this works
 
-  //Get the folders and their associated books, move the pannel to the correct position
-  const footer = document.getElementById("footer_container");
-  document.body.appendChild(footer);
-
-  //clear any existing entries, spilt up the selects into folders, books and misc
-  const SELECT_LIST = document.querySelectorAll("select");
-  for (let i = 0; i < SELECT_LIST.length; i++) {
-    SELECT_LIST[i].innerHTML = "<option>No selection</option>";
-  }
-  let folderSelects = [];
-  let bookSelects = []; //TODO: check if this is redundant
-  let miscFolders = [];
-  for (let i = 0; i < SELECT_LIST.length; i++) {
-    if (SELECT_LIST[i].getAttribute("name").includes("folder")) {
-      folderSelects.push(SELECT_LIST[i]);
-    } else if (SELECT_LIST[i].getAttribute("name").includes("book")) {
-      bookSelects.push(SELECT_LIST[i]);
-    } else {
-      miscFolders.push(SELECT_LIST[i]);
-    }
-  }
-
-  //populate the folder selects with all the folders
-  let selectContents = "";
-  const FOLDER_TITLES = document.getElementsByClassName("book_list_title");
-  for (let i = 0; i < FOLDER_TITLES.length; i++) {
-    const entry = FOLDER_TITLES[i].innerHTML.replace(":", "");
-    selectContents += `<option value = ${entry}>${entry}</option>`;
-  }
-  for (let i = 0; i < folderSelects.length; i++) {
-    folderSelects[i].innerHTML += selectContents;
-  }
-
-  //Add event listeners to each of the folder selects, to detect changes and populate their book folder acordingly
-  for (let i = 0; i < folderSelects.length; i++) {
-    //Check if the folder select has a matching book folder
-    let targetBookSelectName =
-      folderSelects[i].getAttribute("name").substring(0, 2) + "_book_select";
-    let targetBookSelect = document.getElementsByName(targetBookSelectName);
-
-    if (targetBookSelect.length != 0) {
-      folderSelects[i].addEventListener("change", function () {
-        //Find the right folder
-        let bookSelectContents = "";
-        let Folders = document.querySelectorAll("h5");
-        for (let x = 0; x < Folders.length; x++) {
-          if (
-            this.options[this.selectedIndex].text ==
-            Folders[x].innerHTML.replace(":", "")
-          ) {
-            //Once found, go to it's sibling list and collect the books
-            let bookList = Folders[x].nextSibling.childNodes;
-            for (let y = 0; y < bookList.length; y++) {
-              const entry = bookList[y].innerHTML.replace(":", "");
-              bookSelectContents += `<option value = '${entry}'>${entry}</option>`;
+  for (select of SELECT_LIST) {
+    select.innerHTML = "<option>No selection</option>";
+    //Populate the folder list
+    if (select.getAttribute("name").includes("folder")) {
+      for (folder of BOOK_MAP.Books) {
+        const option = document.createElement("option");
+        option.value = option.textContent = folder.Folder;
+        select.appendChild(option);
+      }
+      //Add listener
+      select.addEventListener("change", function () {
+        let targetBookSelect = document.getElementsByName(
+          this.getAttribute("name").substring(0, 2) + "_book_select" //TODO: investigate why using the select var here instead of this allways results in TH
+        );
+        if (typeof targetBookSelect !== "undefined") {
+          //Find the right folder and add the books
+          targetBookSelect[0].innerHTML = "<option>No selection</option>";
+          for (folder of BOOK_MAP.Books) {
+            if (this.options[this.selectedIndex].text == folder.Folder) {
+              for (book of folder.Content) {
+                const option = document.createElement("option");
+                option.value = option.textContent = book.Name + book.ext;
+                targetBookSelect[0].appendChild(option);
+              }
             }
           }
         }
-        //Add the books to the matching book select
-        let bookSelectName =
-          this.getAttribute("name").substring(0, 2) + "_book_select";
-        let targetBookSelect = "";
-        targetBookSelect = document.getElementsByName(bookSelectName);
-        targetBookSelect[0].innerHTML = bookSelectContents;
       });
     }
   }
-
   //Case by case handling for the misc selects
   const Thumb_select = document.getElementById("TH_new_select");
-  Thumb_select.innerHTML = "<option>No selection</option>";
-  Thumb_list = document.getElementById("Thumb_list").children;
-  for (let i = 0; i < Thumb_list.length; i++) {
-    const entry = Thumb_list[i].innerHTML.replace(":", "");
-    Thumb_select.innerHTML += `<option value = '${entry}'>${entry}</option>`;
+  for (image in THUMB_MAP.Images) {
+    const option = document.createElement("option");
+    option.value = option.textContent = image.Name + image.ext;
+    Thumb_select.appendChild(option);
   }
 }
 
