c1210562f945a7a8a5f402a6dc87e7dc2baff986
Camh-git <cameronhillier1@gmail.com>
2023-12-27 22:56:50 +0000

    Feat: WIP dropdown toggle
    Added an event listener to each groups toggle image that toggles the section's visibility
    Modified the CSS rules regarding the dropdowns to be more sensible.
    Replaced the right chevron images with expand more/less

--- Assets/Scripts/Append_management_rows.js
+++ Assets/Scripts/Append_management_rows.js
@@ -1,6 +1,7 @@
 function get_rows() {
   Rows = document.getElementById("Row_list").children;
   for (group of Rows) {
+    //Add the new forms
     const new_row = document.createElement("section");
     new_row.classList += "Management-row";
     $(new_row).load(
@@ -10,6 +11,20 @@ function get_rows() {
       )}.html`
     );
     group.appendChild(new_row);
+
+    //Add the dropdown to the forms
+    group
+      .getElementsByTagName("img")[0]
+      .addEventListener("click", function (e) {
+        let section = e.target.parentNode.getElementsByTagName("section")[0];
+        if (section.style.display == "inline") {
+          section.style.display = "none";
+          e.target.src = "../Assets/Images/expand_more_white_24dp.svg";
+        } else {
+          section.style.display = "inline";
+          e.target.src = "../Assets/Images/expand_less_white_24dp.svg";
+        }
+      });
   }
 }
 get_rows();
