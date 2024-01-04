ae82e3ff81b9208f3dd93a0b8fd547aae96bff35
Camh-git <cameronhillier1@gmail.com>
2024-01-04 21:04:44 +0000

    fix: incorrect component ID
    fixed some missing IDs in one of the management components
    Experiment with setting event listeners using jquery

---
+++ Assets/Scripts/Set_management_listeners-import.js
@@ -0,0 +1,236 @@
+import { Call_and_display } from "./Call_and_display.js";
+
+function Assign_submit_actions() {
+  //Get the submit btns/forms and assign their actions to the functions
+  //Manage books
+  $("DS_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Folder = event.target.children[2];
+    let Book = event.target.children[5];
+    Call_and_display(
+      `${ADDRESS}delete-book/${
+        Book.options[Book.selectedIndex].innerHTML.split(".")[0]
+      }&&${Book.options[Book.selectedIndex].innerHTML.split(".")[1]}&&${
+        Folder.options[Folder.selectedIndex].innerHTML
+      }`,
+      true
+    );
+  });
+  $("RS_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Folder = event.target.children[2];
+    let Book = event.target.children[5];
+    let New_name = event.target.children[7];
+    Call_and_display(
+      `${ADDRESS}rename-book/${
+        Book.options[Book.selectedIndex].innerHTML.split(".")[0]
+      }&&${Book.options[Book.selectedIndex].innerHTML.split(".")[1]}&&${
+        Folder.options[Folder.selectedIndex].innerHTML
+      }&&${New_name.value}`,
+      true
+    );
+  });
+
+  //Manage folders
+  $("DF_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Folder = event.target.children[1];
+    let Delete = event.target.children[5];
+    if (Delete.checked) {
+      Delete = true;
+    } else {
+      Delete = false;
+    }
+    Call_and_display(
+      `${ADDRESS}delete-folder/${
+        Folder.options[Folder.selectedIndex].innerHTML
+      }&&${Delete}`,
+      true
+    );
+  });
+
+  $("RF_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Folder = event.target.children[1];
+    let New_name = event.target.children[2];
+    Call_and_display(
+      `${ADDRESS}rename-folder/${
+        Folder.options[Folder.selectedIndex].innerHTML
+      }&&${New_name.value}`,
+      true
+    );
+  });
+
+  //Manage Library
+  $("CF_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Name = event.target.children[1];
+    Call_and_display(`${ADDRESS}create-folder/${Name.value}`, true);
+  });
+
+  $("MV_folder_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Book = event.target.children[5];
+    let Old_folder = event.target.children[2];
+    let New_folder = event.target.children[8];
+
+    Call_and_display(
+      `${ADDRESS}move-book-to-folder/${
+        Book.options[Book.selectedIndex].innerHTML.split(".")[0]
+      }&&${Book.options[Book.selectedIndex].innerHTML.split(".")[1]}&&${
+        Old_folder.options[Old_folder.selectedIndex].innerHTML
+      }&&${New_folder.options[New_folder.selectedIndex].innerHTML}`,
+      true
+    );
+  });
+
+  //Manage thumbnails
+  $("TH_select_form").bind("submit", (event) => {
+    event.preventDefault();
+    let Folder = event.target.children[2];
+    let Book = event.target.children[5];
+    let Image = event.target.children[8];
+    Call_and_display(
+      `${ADDRESS}reassign-thumb/${
+        Folder.options[Folder.selectedIndex].innerHTML
+      }&&${Book.options[Book.selectedIndex].innerHTML}&&
+        ${Image.options[Image.selectedIndex].innerHTML}`
+    );
+  });
+  $("TH_upload_form").bind("submit", (event) => {});
+  $("TH_rename_form").bind("submit", (event) => {
+    event.preventDefault();
+    let target = event.target.children[2];
+    let newName = event.target.children[5];
+    console.log(newName.value);
+    Call_and_display(
+      `${ADDRESS}rename-thumb/${
+        target.options[target.selectedIndex].innerHTML
+      }&&${newName.value}`
+    );
+  });
+  $("TH_format_form").bind("submit", (event) => {
+    event.preventDefault();
+    let regen = event.target.children[4];
+    let rmManual = event.target.children[7];
+    if (regen.checked) {
+      regen = true;
+    } else {
+      regen = false;
+    }
+    if (rmManual.checked) {
+      rmManual = true;
+    } else {
+      rmManual = false;
+    }
+    Call_and_display(`${ADDRESS}clear-thumbs/${regen}&&${rmManual}`);
+  });
+
+  //Misc options
+  $("DL_toggle_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[7];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=DL_toggle]:checked"
+    ).value;
+    Call_and_display(`${ADDRESS}toggle-dl/${Toggle}&&${Code.value}`);
+  });
+
+  $("TOGR_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[7];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=Reader_toggle]:checked"
+    ).value;
+    Call_and_display(`${ADDRESS}toggle-readers/${Toggle}&&${Code.value}`);
+  });
+
+  $("IP_form").bind("submit", (event) => {
+    event.preventDefault();
+    const IP = event.target.children[7];
+    const Code = event.target.children[8];
+    const List = document.querySelector(
+      "input[type='radio'][name=IP_list_choice]:checked"
+    ).value;
+    const Action = document.querySelector(
+      "input[type='radio'][name=IP_list_add_or_rm]:checked"
+    ).value;
+    Call_and_display(
+      `${ADDRESS}manage-acls/${IP.value}&&${List}&&${Action}&&${Code.value}`,
+      false,
+      true,
+      Code
+    );
+  });
+
+  $("IPR_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[11];
+    const List = document.querySelector(
+      "input[type='radio'][name=IPR_list_choice]:checked"
+    ).value;
+    Call_and_display(
+      `${ADDRESS}toggle-lists/${List}&&${Code.value}`,
+      false,
+      true,
+      Code
+    );
+  });
+
+  //Management options
+  $("MNT_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[8];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=MNT_toggle]:checked"
+    ).value;
+    Call_and_display(
+      `${ADDRESS}/toggle-management/${Toggle}&&MANAGEMENT&&${Code}`
+    );
+  });
+
+  $("MNU_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[8];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=MNU_toggle]:checked"
+    ).value;
+    Call_and_display(`${ADDRESS}/toggle-management/${Toggle}&&UPLOAD&&${Code}`);
+  });
+
+  $("MND_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[8];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=MND_toggle]:checked"
+    ).value;
+    Call_and_display(`${ADDRESS}/toggle-management/${Toggle}&&DELETE&&${Code}`);
+  });
+
+  $("MNR_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[8];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=MNR_toggle]:checked"
+    ).value;
+    Call_and_display(`${ADDRESS}/toggle-management/${Toggle}&&RENAME&&${Code}`);
+  });
+
+  $("MNM_form").bind("submit", (event) => {
+    event.preventDefault();
+    const Code = event.target.children[8];
+    const Toggle = document.querySelector(
+      "input[type='radio'][name=MNM_toggle]:checked"
+    ).value;
+    Call_and_display(`${ADDRESS}/toggle-management/${Toggle}&&MOVE&&${Code}`);
+  });
+
+  //Allow the user to close the response popup
+  $("Req_status_modal").bind("click", (event) => {
+    DISPLAY.style.display = "none";
+  });
+}
+
+const ADDRESS = "http://192.168.1.110:5000/";
+const DISPLAY = document.getElementById("Req_status_modal");
+Assign_submit_actions();
